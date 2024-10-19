9ebb5177ac68e7d36951beaf732abf6de9cf4a3a
Mathys Lopinto <lopintomathys@pm.me>
2024-10-19 21:55:00 +0200

    Bangshell + Reformat + Config verification + Requirments
    Add python bangshell on file who missing it
    Reformat code
    Add an function for verify the config file
    Update the requirments.txt file

--- remoteDockersMgrConfig.py
+++ remoteDockersMgrConfig.py
@@ -1,30 +1,126 @@
 #!/usr/bin/env python3
- 
-import yaml
 
-config={}
+import yaml
+import ipaddress
+import os
+import re
+
+config = {}
+
 
 def config():
-    global config 
+    global config
     return config
 
 
 def config_load():
     global config
-    config=None
-    with open("config.yaml", 'r') as stream:
-        try:
-            config = (yaml.safe_load(stream))
-            #print(config)
-        except yaml.YAMLError as exc:
-            print(exc)
-    #print(config)
-    return config
+    config = None
+    config_temp = {}
+    try:
+        with open("config.yaml", 'r') as stream:
+            try:
+                config_temp = yaml.safe_load(stream)
+            except yaml.YAMLError as exc:
+                print(f"YAML parsing error: {exc}")
+                return None
+
+        config_valid, config_message = check_config(config_temp)
+        if not config_valid:
+            print(f"Configuration error:\n{config_message}")
+        else:
+            config = config_temp
+            print(f"Configuration loaded successfully")
+        return config
+    except FileNotFoundError:
+        print("Config file 'config.yaml' not found.")
+        return None
+    except PermissionError:
+        print("Permission denied when trying to read 'config.yaml'.")
+        return None
+    except Exception as e:
+        print(f'Unexpected error reading config file: {e}')
+        return None
+
+def check_config(conf):
+    if conf is None:
+        return False, "Configuration not loaded"
+
+    errors = []
+
+    # Main category checks
+    if not isinstance(conf.get('debugmode'), bool):
+        errors.append("debugmode must be a boolean")
+
+    if not isinstance(conf.get('checkserverlogin'), bool):
+        errors.append("checkserverlogin must be a boolean")
+
+    if not isinstance(conf.get('checkservermem'), bool):
+        errors.append("checkservermem must be a boolean")
+
+    if not isinstance(conf.get('httpserverhost'), str):
+        errors.append("httpserverhost must be a string")
+
+    if not isinstance(conf.get('httpserverport'), int) or not 1 <= conf.get('httpserverport') <= 65535:
+        errors.append("httpserverport must be a valid port number (1-65535)")
+
+    if not isinstance(conf.get('secret'), str) or len(conf.get('secret', '')) < 8:
+        errors.append("secret must be a string with at least 8 characters")
+
+    if not isinstance(conf.get('servers'), list):
+        errors.append("servers must be a list")
+    else:
+        # Subcategory server checks
+        for idx, server in enumerate(conf.get('servers', [])):
+            server_errors = []
+
+            if not isinstance(server.get('name'), str) or not server.get('name'):
+                server_errors.append("name must be a non-empty string")
+
+            try:
+                ipaddress.ip_address(server.get('host', ''))
+            except ValueError:
+                server_errors.append("host must be a valid IP address")
+
+            if not isinstance(server.get('port'), int) or not 1 <= server.get('port') <= 65535:
+                server_errors.append("port must be a valid port number (1-65535)")
+
+            if not isinstance(server.get('user'), str) or not server.get('user'):
+                server_errors.append("user must be a non-empty string")
+
+            if server.get('password') and not isinstance(server.get('password'), str):
+                server_errors.append("password must be a string or empty")
+
+            if server.get('privatekeyfile'):
+                if not isinstance(server.get('privatekeyfile'), str) or not os.path.exists(server.get('privatekeyfile')):
+                    server_errors.append("privatekeyfile must be a valid path to an existing file")
+
+            if not isinstance(server.get('host_public'), str) or not re.match(r'^[a-zA-Z0-9.-]+$', server.get('host_public', '')):
+                server_errors.append("host_public must be a valid FQDN")
+
+            if not isinstance(server.get('host_public_baseport'), int) or not 1 <= server.get('host_public_baseport') <= 65535:
+                server_errors.append("host_public_baseport must be a valid port number (1-65535)")
+
+            if not isinstance(server.get('maxclient'), int) or server.get('maxclient') < 1:
+                server_errors.append("maxclient must be a positive integer")
+
+            if not isinstance(server.get('enabled'), bool):
+                server_errors.append("enabled must be a boolean")
+
+            if server_errors:
+                errors.append(f"Errors in server {idx + 1} ({server.get('name', 'unnamed')}):")
+                errors.extend(f"  - {error}" for error in server_errors)
+
+    if errors:
+        return False, "\n".join(errors)
+    else:
+        return True, "Configuration is valid"
+
 
 def config_get_servers():
     global config
-    servers=[]
-    if None!=config:
+    servers = []
+    if None != config:
         for server in config['servers']:
             servers.append(server['name'])
     return servers
@@ -32,8 +128,8 @@ def config_get_servers():
 
 def config_get_server(name):
     global config
-    servers=[]
-    if None!=config:
+    servers = []
+    if None != config:
         for server in config['servers']:
             if name == server['name']:
                 return server
--- remoteDockersMgrHttp.py
+++ remoteDockersMgrHttp.py
@@ -1,33 +1,39 @@
+#!/usr/bin/env python3
+
+import base64
+import traceback
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import json
 from urllib.parse import urlparse, parse_qs
-from remoteDockersMgrDocker import *
+
 from remoteDockersMgrConfig import *
-import re
-import ssl
-import base64
-import traceback
-    
-    
+from remoteDockersMgrDocker import *
+
+
 def clean_uid(uid):
     return ''.join(filter(lambda c: c in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVXYZ1234567890-_", uid))
+
 
 def clean_string(text):
     return ''.join(i for i in text if i.isprintable())
 
+
 def clean_param(uid):
     return ''.join(filter(lambda c: c in "ABCDEFGHIJKLMNOPQRSTUVWXYabcdefghijklmnopqrstuvwxyz1234567890_-", uid))
+
 
 def clean_image(image):
     return ''.join(filter(lambda c: c in "ABCDEFGHIJKLMNOPQRSTUVWXYabcdefghijklmnopqrstuvwxyz1234567890_-:/.", image))
 
+
 def clean_base64(image):
     return ''.join(filter(lambda c: c in "ABCDEFGHIJKLMNOPQRSTUVWXYabcdefghijklmnopqrstuvwxyz1234567890+=/", image))
-   
+
+
 def clean_dockerid(id):
     return ''.join(filter(lambda c: c in "ABCDEFabcdef1234567890", id))
-   
-    
+
+
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     # Default response
     def do_GET_default(self):
@@ -35,81 +41,81 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         self.end_headers()
         str = "<html>Remote Dockers Mgr: Ready</html>"
         self.wfile.write(str.encode(encoding='utf_8'))
-        
+
     # Error response
     def do_GET_error(self, msg):
         self.send_response(403)
         self.end_headers()
-        str = "<html>"+msg+"</html>"
+        str = "<html>" + msg + "</html>"
         self.wfile.write(str.encode(encoding='utf_8'))
 
     # Parse parameters
     def param_get(self, param):
         query_components = parse_qs(urlparse(self.path).query)
-        #print(query_components)
-        paramvalue= ""
+        # print(query_components)
+        paramvalue = ""
         if param in query_components:
             paramvalue = query_components[param][0]
-            #print(paramvalue)
-            #print("found "+param+"="+paramvalue)
+            # print(paramvalue)
+            # print("found "+param+"="+paramvalue)
         return paramvalue
 
     def param_get_image(self, param):
         imageraw = self.param_get('image')
         image = clean_image(imageraw)
         return image
-     
+
     def checkSecret(self):
-        #print("Check secret")
-        if (config()['secret']=="") or (config()['secret'])==None:
+        # print("Check secret")
+        if (config()['secret'] == "") or (config()['secret']) == None:
             return True
         secretraw = self.param_get('secret')
         secretbase64 = clean_base64(secretraw)
         secret = base64.b64decode(secretbase64)
-        return secret==config()['secret']
-    
-    def do_GET_serverlist(self):  
+        return secret == config()['secret']
+
+    def do_GET_serverlist(self):
         status = config_get_servers()
         json_str = json.dumps(status)
-        #print("do_GET_serverlist: ["+json_str+"]")
+        # print("do_GET_serverlist: ["+json_str+"]")
         self.send_response(200)
         self.send_header('Content-Type', 'application/json')
         self.end_headers()
-        self.wfile.write(json_str.encode(encoding='utf_8')) 
+        self.wfile.write(json_str.encode(encoding='utf_8'))
 
     def do_GET_dockerps(self):
         serverraw = self.param_get('server')
         server = clean_string(serverraw)
-        #print(server)
-        #print("do_GET_dockerps: ["+server+"]")
+        # print(server)
+        # print("do_GET_dockerps: ["+server+"]")
         serverentry = config_get_server(server)
-        if (not (server in  config_get_servers())) or (serverentry==None):
+        if (not (server in config_get_servers())) or (serverentry == None):
             self.do_GET_error("Bad server name")
             return
-    
+
         ret = remote_docker_ps_all(serverentry)
-        #print(ret)
+        # print(ret)
         json_str = json.dumps(ret)
         self.send_response(200)
         self.send_header('Content-Type', 'application/json')
         self.end_headers()
-        self.wfile.write(json_str.encode(encoding='utf_8'))        
+        self.wfile.write(json_str.encode(encoding='utf_8'))
 
     def do_GET_dockerrun(self):
         serverraw = self.param_get('server')
         server = clean_string(serverraw)
         serverentry = config_get_server(server)
-        if (not (server in  config_get_servers())) or (serverentry==None):
+        if (not (server in config_get_servers())) or (serverentry == None):
             self.do_GET_error("Bad server name")
             return
-        
+
         imageraw = self.param_get('image')
         image = clean_image(imageraw)
-        #print(image)
+        # print(image)
 
         nameraw = self.param_get('name')
         name = clean_image(nameraw)
-        #print(name)
+        # print(name)
 
         portraw = self.param_get('port')
         port = clean_image(portraw)
@@ -123,16 +129,16 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-Type', 'application/json')
         self.end_headers()
-        self.wfile.write(json_str.encode(encoding='utf_8'))   
+        self.wfile.write(json_str.encode(encoding='utf_8'))
 
     def do_GET_dockerdestroy(self):
         serverraw = self.param_get('server')
         server = clean_string(serverraw)
         serverentry = config_get_server(server)
-        if (not (server in  config_get_servers())) or (serverentry==None):
+        if (not (server in config_get_servers())) or (serverentry == None):
             self.do_GET_error("Bad server name")
             return
-        
+
         idraw = self.param_get('id')
         id = clean_dockerid(idraw)
 
@@ -141,16 +147,15 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-Type', 'application/json')
         self.end_headers()
-        self.wfile.write(json_str.encode(encoding='utf_8'))   
+        self.wfile.write(json_str.encode(encoding='utf_8'))
 
-
-    def do_GET(self):  
+    def do_GET(self):
         try:
             print("[+] HTTP GET %s" % clean_string(self.path), flush=True)
             if not self.checkSecret():
                 self.do_GET_error("Bad shared secret")
-                return   
-                     
+                return
+
             if self.path.startswith("/serverlist"):
                 self.do_GET_serverlist()
             elif self.path.startswith("/dockerps"):
